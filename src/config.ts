9a55fd7b091041e7ca55f088438d11c65d8c5a93
budizulkoplo <139447405+budizulkoplo@users.noreply.github.com>
2023-08-11 09:17:28 +0700

    go public

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const SITE = {
-    // Your site's detail?
-    name: 'Me',
-    title: 'Astro - Me',
-    description: 'Crisp, minimal, personal portfolio theme for Astro',
-    url: 'https://astro-me.vercel.app',
-    githubUrl: 'https://github.com/one-aalam/astro-me'
-    // description ?
-}
